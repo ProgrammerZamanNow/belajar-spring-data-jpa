ce3ef32388bd07992dcc859571a3e3ef439ed7b8
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-05-12 12:33:28 +0700

    Modifying

--- src/main/java/programmerzamannow/springdata/jpa/repository/ProductRepository.java
+++ src/main/java/programmerzamannow/springdata/jpa/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,6 +15,14 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Modifying
+    @Query("delete from Product p where p.name = :name")
+    int deleteProductUsingName(@Param("name") String name);
+
+    @Modifying
+    @Query("update Product p set p.price = 0 where p.id = :id")
+    int updateProductPriceToZero(@Param("id") Long id);
 
     @Query(
             value = "select p from Product p where p.name like :name or p.category.name like :name",
--- src/test/java/programmerzamannow/springdata/jpa/repository/ProductRepositoryTest.java
+++ src/test/java/programmerzamannow/springdata/jpa/repository/ProductRepositoryTest.java
@@ -171,4 +171,19 @@ class ProductRepositoryTest {
         assertEquals(2, products.getTotalPages());
         assertEquals(2, products.getTotalElements());
     }
+
+    @Test
+    void modifying() {
+        transactionOperations.executeWithoutResult(transactionStatus -> {
+            int total = productRepository.deleteProductUsingName("Wrong");
+            assertEquals(0, total);
+
+            total = productRepository.updateProductPriceToZero(1L);
+            assertEquals(1, total);
+
+            Product product = productRepository.findById(1L).orElse(null);
+            assertNotNull(product);
+            assertEquals(0L, product.getPrice());
+        });
+    }
 }
