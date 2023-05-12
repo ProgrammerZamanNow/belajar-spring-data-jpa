22201b31ffb2a54b691e7b5a5d2e61340c8bd6ca
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-05-12 13:20:22 +0700

    Specification

--- src/main/java/programmerzamannow/springdata/jpa/repository/ProductRepository.java
+++ src/main/java/programmerzamannow/springdata/jpa/repository/ProductRepository.java
@@ -5,10 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,7 +17,7 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Product> findFirstByIdEquals(Long id);
--- src/test/java/programmerzamannow/springdata/jpa/repository/ProductRepositoryTest.java
+++ src/test/java/programmerzamannow/springdata/jpa/repository/ProductRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.*;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.transaction.support.TransactionOperations;
 import programmerzamannow.springdata.jpa.entity.Category;
 import programmerzamannow.springdata.jpa.entity.Product;
@@ -235,5 +236,20 @@ class ProductRepositoryTest {
             product.setPrice(10_000_000L);
             productRepository.save(product);
         });
+    }
+
+    @Test
+    void specification() {
+        Specification<Product> specification= (root, criteriaQuery, criteriaBuilder) -> {
+            return criteriaQuery.where(
+                    criteriaBuilder.or(
+                            criteriaBuilder.equal(root.get("name"), "Apple iPhone 14 Pro Max"),
+                            criteriaBuilder.equal(root.get("name"), "Apple iPhone 13 Pro Max")
+                    )
+            ).getRestriction();
+        };
+
+        List<Product> products = productRepository.findAll(specification);
+        assertEquals(2, products.size());
     }
 }
