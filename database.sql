e1aa829f50c34d891b997133d3a16219d825d36f
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-05-12 08:17:02 +0700

    belajar spring data jpa

---
+++ database.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE belajar_spring_data_jpa;
+
+USE belajar_spring_data_jpa;
---
+++ src/main/java/programmerzamannow/springdata/jpa/BelajarSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package programmerzamannow.springdata.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringDataJpaApplication.class, args);
+	}
+
+}
